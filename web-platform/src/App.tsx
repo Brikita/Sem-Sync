92f6176e7ff526f707f23b0796f07da58697198a
Brian Mwangi <briankinyua0101@gmail.com>
2026-01-07 11:51:58 +0300

    feat: implement routing for Landing, Login, and Register pages

--- web-platform/src/App.tsx
+++ web-platform/src/App.tsx
@@ -1,9 +1,18 @@
-import LandingPage from "./pages/LandingPage"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/auth/LoginPage";
+import RegisterPage from "./pages/auth/RegisterPage";
 
 function App() {
   return (
-    <LandingPage />
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
