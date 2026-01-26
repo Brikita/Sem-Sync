a98bdc123cb55e54a6e798646a0382646ed115bd
Brian Mwangi <briankinyua0101@gmail.com>
2026-01-26 22:05:32 +0300

    wow

--- mobile-app/app/src/main/java/com/example/semsync/MainActivity.kt
+++ mobile-app/app/src/main/java/com/example/semsync/MainActivity.kt
@@ -8,6 +8,7 @@ import com.example.semsync.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
