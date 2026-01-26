4a35d229135d75edc96df9f90f8d8a5d6c726542
Brian Mwangi <briankinyua0101@gmail.com>
2026-01-26 22:00:35 +0300

    feat: add initial TimetaleFragment

---
+++ mobile-app/app/src/main/java/com/example/semsync/TimetableFragment.kt
@@ -0,0 +1,18 @@
+package com.example.semsync
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class TimetableFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_timetable, container, false)
+    }
+}
